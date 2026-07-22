3cd676b704209c1264a00cd4048abd8a672939c5
Amin <arsindhurakar@gmail.com>
2026-07-22 23:51:28 +0545

    refactor: replaced server start info with logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
+import { logger } from "@/logger/index.js";
 import app from "./app.js";
 import { config } from "./config/index.js";
 
 const PORT = config.port ?? 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  logger.info(
+    {
+      port: PORT,
+      env: process.env.NODE_ENV,
+    },
+    "Server started",
+  );
 });
